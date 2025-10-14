c2c3cca6f9ca023e97eacab9a7a70b295d681e69
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-10-15 01:11:17 +0200

    Add methods to cert repo

--- src/main/java/org/cybersecurity/repositories/pki/CertificateRepository.java
+++ src/main/java/org/cybersecurity/repositories/pki/CertificateRepository.java
@@ -3,4 +3,12 @@ package org.cybersecurity.repositories.pki;
 import org.cybersecurity.model.pki.CertificateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CertificateRepository extends JpaRepository<CertificateEntity, Long> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface CertificateRepository extends JpaRepository<CertificateEntity, Long> {
+     List<CertificateEntity> findByIssuerId(Long issuerId);
+     List<CertificateEntity> findByIssuerIdAndStatus(Long issuerId, String status);
+     Optional<CertificateEntity> findBySerialHex(String serialHex);
+}
+
